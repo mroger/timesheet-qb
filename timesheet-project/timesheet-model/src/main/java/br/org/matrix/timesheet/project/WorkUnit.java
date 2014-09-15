e4edb26bfc94d86045e733819d2335db3650c568
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-09-15 00:07:32 -0300

    first commit

---
+++ timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/project/WorkUnit.java
@@ -0,0 +1,11 @@
+package br.org.matrix.timesheet.project;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+public class WorkUnit {
+
+	private LocalDate date;
+	private LocalTime startTime;
+	private LocalTime stopTime;
+}
