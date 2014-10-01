63e0506b96f3ce3fea75409ce8e3cfbc25c33a65
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-09-30 23:41:47 -0300

    New search methods.

--- timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/time/WorkRepository.java
+++ timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/time/WorkRepository.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import org.joda.time.LocalDate;
 
+import br.org.matrix.timesheet.project.Employee;
+
 public interface WorkRepository {
 
 	void store(WorkPeriod workPeriod);
 
 	List<WorkPeriod> findByDate(LocalDate date);
+
+	List<WorkPeriod> findByEmployee(Employee employee);
+
+	List<WorkPeriod> findByDateAndEmployee(LocalDate yesterday, Employee employee1);
 }
