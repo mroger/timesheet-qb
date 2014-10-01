fa9bfdb9ee30cf7635a7e560ec9d11b7ff0420a1
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-09-30 23:42:38 -0300

    New test methods.

--- timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkTest.java
+++ timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkTest.java
@@ -3,6 +3,9 @@ package br.org.matrix.timesheet.time;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.equalTo;
+
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -51,7 +54,7 @@ public class WorkTest {
 	}
 	
 	@Test
-	public void shouldFindStoredWorkPeriodEvenIfDifferentObject() {
+	public void shouldFindStoredWorkPeriodEvenIfDifferentObjects() {
 		LocalDate date = today();
 		WorkPeriod workPeriodStored = new WorkPeriod(date, time(11, 0), time(15, 0), new Employee(1, "emp01"));
 		WorkPeriod workPeriodEqualToStored = new WorkPeriod(date, time(11, 0), time(15, 0), new Employee(1, "emp01"));
@@ -64,7 +67,85 @@ public class WorkTest {
 	}
 	
 	@Test
-	public void shouldFindStoredWorkPeriodForDateAndClient() {
+	public void shouldFindStoredWorkPeriodByDate() {
+		LocalDate today = today();
+		LocalDate yesterday = yesterday();
+		WorkPeriod workedToday1 = new WorkPeriod(today, time(8, 0), time(12, 0), new Employee(1, "emp01"));
+		WorkPeriod workedToday2 = new WorkPeriod(today, time(13, 0), time(17, 0), new Employee(1, "emp01"));
+		WorkPeriod workedYesterday1 = new WorkPeriod(yesterday, time(8, 0), time(12, 0), new Employee(1, "emp01"));
+		WorkPeriod workedYesterday2 = new WorkPeriod(yesterday, time(13, 0), time(17, 0), new Employee(1, "emp01"));
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedToday1);
+		workRepository.store(workedToday2);
+		workRepository.store(workedYesterday1);
+		workRepository.store(workedYesterday2);
+		
+		List<WorkPeriod> workPeriods = workRepository.findByDate(yesterday);
+		
+		assertThat(workPeriods.size(), equalTo(2));
+		assertTrue(workPeriods.contains(workedYesterday1));
+		assertTrue(workPeriods.contains(workedYesterday2));
+	}
+	
+	@Test
+	public void shouldFindStoredWorkPeriodByEmployee() {
+		LocalDate today = today();
+		Employee employee1 = new Employee(1, "emp01");
+		WorkPeriod workedToday = new WorkPeriod(today, time(8, 0), time(12, 0), employee1);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedToday);
+		
+		List<WorkPeriod> workPeriods = workRepository.findByEmployee(employee1);
+		
+		assertTrue(workPeriods.contains(workedToday));
+	}
+	
+	@Test
+	public void shouldFindStoredWorkPeriodByDateAndEmployee() {
+		LocalDate today = today();
+		LocalDate yesterday = yesterday();
+		Employee employee1 = new Employee(1, "emp01");
+		Employee employee2 = new Employee(2, "emp02");
+		
+		WorkPeriod workedToday1Eployee1 = new WorkPeriod(today, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedToday2Eployee1 = new WorkPeriod(today, time(13, 0), time(17, 0), employee1);
+		WorkPeriod workedYesterday1Eployee1 = new WorkPeriod(yesterday, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedYesterday2Eployee1 = new WorkPeriod(yesterday, time(13, 0), time(17, 0), employee1);
+		
+		WorkPeriod workedToday1Eployee2 = new WorkPeriod(today, time(8, 0), time(12, 0), employee2);
+		WorkPeriod workedToday2Eployee2 = new WorkPeriod(today, time(13, 0), time(17, 0), employee2);
+		WorkPeriod workedYesterday1Eployee2 = new WorkPeriod(yesterday, time(8, 0), time(12, 0), employee2);
+		WorkPeriod workedYesterday2Eployee2 = new WorkPeriod(yesterday, time(13, 0), time(17, 0), employee2);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedToday1Eployee1);
+		workRepository.store(workedToday2Eployee1);
+		workRepository.store(workedYesterday1Eployee1);
+		workRepository.store(workedYesterday2Eployee1);
+		
+		workRepository.store(workedToday1Eployee2);
+		workRepository.store(workedToday2Eployee2);
+		workRepository.store(workedYesterday1Eployee2);
+		workRepository.store(workedYesterday2Eployee2);
+		
+		List<WorkPeriod> workPeriods = workRepository.findByDateAndEmployee(yesterday, employee1);
+		assertThat(workPeriods.size(), equalTo(2));
+		assertThat(workPeriods.get(0).getDate(), equalTo(yesterday));
+		assertThat(workPeriods.get(1).getDate(), equalTo(yesterday));
+		assertThat(workPeriods.get(0).getEmployee(), equalTo(employee1));
+		assertThat(workPeriods.get(1).getEmployee(), equalTo(employee1));
+		
+	}
+
+	@Test
+	public void shouldFindStoredWorkPeriodByClient() {
+		
+	}
+	
+	@Test
+	public void shouldFindStoredWorkPeriodByDateAndClient() {
 		
 	}
 	
@@ -75,6 +156,16 @@ public class WorkTest {
 	
 	@Test
 	public void shouldNotAcceptWorkPeriodThatOverlapsWithExistingWorkPeriod() {
+		
+	}
+	
+	@Test
+	public void shouldFindWorkPeriodOfAClientInAGivenMonth() {
+		
+	}
+	
+	@Test
+	public void shouldFindWorkPeriodOfAClientBetweenTwoMonths() {
 		
 	}
 
@@ -88,5 +179,9 @@ public class WorkTest {
 
 	private LocalTime time(int hour, int minute) {
 		return new LocalTime(hour, minute);
+	}
+	
+	private LocalDate yesterday() {
+		return new LocalDate().minusDays(1);
 	}
 }
