ba3ea9ecff965fbb4ae941952df4eebf6952f1ea
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-10-01 22:42:21 -0300

    New tests

--- timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkTest.java
+++ timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkTest.java
@@ -13,6 +13,7 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.junit.Test;
 
+import br.org.matrix.timesheet.project.DateIntervalOvelapsException;
 import br.org.matrix.timesheet.project.Employee;
 
 public class WorkTest {
@@ -138,6 +139,121 @@ public class WorkTest {
 		assertThat(workPeriods.get(1).getEmployee(), equalTo(employee1));
 		
 	}
+	
+	@Test
+	public void shouldFindStoredWorkPeriodForDateInterval() {
+		LocalDate date1 = createDate(2000, 1, 1);
+		LocalDate date2 = createDate(2000, 1, 2);
+		LocalDate date3 = createDate(2000, 1, 3);
+		LocalDate date4 = createDate(2000, 1, 4);
+		LocalDate date5 = createDate(2000, 1, 5);
+		LocalDate date6 = createDate(2000, 1, 6);
+		Employee employee1 = new Employee(1, "emp01");
+		
+		WorkPeriod workedDate1Eployee1 = new WorkPeriod(date1, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate2Eployee1 = new WorkPeriod(date2, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate3Eployee1 = new WorkPeriod(date3, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate4Eployee1 = new WorkPeriod(date4, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate5Eployee1 = new WorkPeriod(date5, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate6Eployee1 = new WorkPeriod(date6, time(8, 0), time(12, 0), employee1);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedDate1Eployee1);
+		workRepository.store(workedDate2Eployee1);
+		workRepository.store(workedDate3Eployee1);
+		workRepository.store(workedDate4Eployee1);
+		workRepository.store(workedDate5Eployee1);
+		workRepository.store(workedDate6Eployee1);
+		
+		List<WorkPeriod> workPeriods = workRepository.findByDateInterval(date2, date5);
+		assertThat(workPeriods.size(), equalTo(4));
+		assertTrue(workPeriods.contains(workedDate2Eployee1));
+		assertTrue(workPeriods.contains(workedDate3Eployee1));
+		assertTrue(workPeriods.contains(workedDate4Eployee1));
+		assertTrue(workPeriods.contains(workedDate5Eployee1));
+	}
+	
+	@Test
+	public void shouldFindStoredWorkPeriodForDateIntervalAndEmployee() {
+		LocalDate date1 = createDate(2000, 1, 1);
+		LocalDate date2 = createDate(2000, 1, 2);
+		LocalDate date3 = createDate(2000, 1, 3);
+		LocalDate date4 = createDate(2000, 1, 4);
+		LocalDate date5 = createDate(2000, 1, 5);
+		LocalDate date6 = createDate(2000, 1, 6);
+		Employee employee1 = new Employee(1, "emp01");
+		Employee employee2 = new Employee(2, "emp02");
+		
+		WorkPeriod workedDate1Eployee1 = new WorkPeriod(date1, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate2Eployee1 = new WorkPeriod(date2, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate3Eployee1 = new WorkPeriod(date3, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate4Eployee1 = new WorkPeriod(date4, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate5Eployee1 = new WorkPeriod(date5, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate6Eployee1 = new WorkPeriod(date6, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedDate1Eployee2 = new WorkPeriod(date1, time(8, 0), time(12, 0), employee2);
+		WorkPeriod workedDate2Eployee2 = new WorkPeriod(date2, time(8, 0), time(12, 0), employee2);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedDate1Eployee1);
+		workRepository.store(workedDate2Eployee1);
+		workRepository.store(workedDate3Eployee1);
+		workRepository.store(workedDate4Eployee1);
+		workRepository.store(workedDate5Eployee1);
+		workRepository.store(workedDate6Eployee1);
+		workRepository.store(workedDate1Eployee2);
+		workRepository.store(workedDate2Eployee2);
+		
+		List<WorkPeriod> workPeriods = workRepository.findByDateIntervalAndEmployee(date2, date4, employee1);
+		assertThat(workPeriods.size(), equalTo(3));
+		assertTrue(workPeriods.contains(workedDate2Eployee1));
+		assertTrue(workPeriods.contains(workedDate3Eployee1));
+		assertTrue(workPeriods.contains(workedDate4Eployee1));
+	}
+	
+	private LocalDate createDate(int year, int month, int day) {
+		return new LocalDate(year, month, day);
+	}
+	
+	@Test(expected=DateIntervalOvelapsException.class)
+	public void shouldNotAcceptWorkPeriodThatOverlapsWithExistingWorkPeriodForTheSameEmployee() {
+		LocalDate today = today();
+		Employee employee1 = new Employee(1, "emp01");
+		
+		WorkPeriod workedInterval1Eployee1 = new WorkPeriod(today, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedInterval2Eployee1 = new WorkPeriod(today, time(10, 0), time(15, 0), employee1);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedInterval1Eployee1);
+		workRepository.store(workedInterval2Eployee1);
+	}
+	
+	@Test
+	public void shouldAcceptWorkPeriodThatOverlapsWithExistingWorkPeriodForTheSameEmployeeAndDifferentDates() {
+		LocalDate yesterday = yesterday();
+		LocalDate today = today();
+		Employee employee1 = new Employee(1, "emp01");
+		
+		WorkPeriod workedInterval1Eployee1 = new WorkPeriod(yesterday, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedInterval2Eployee1 = new WorkPeriod(today, time(10, 0), time(15, 0), employee1);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedInterval1Eployee1);
+		workRepository.store(workedInterval2Eployee1);
+	}
+	
+	@Test
+	public void shouldAcceptWorkPeriodThatOverlapsWithExistingWorkPeriodForDifferentEmployees() {
+		LocalDate today = today();
+		Employee employee1 = new Employee(1, "emp01");
+		Employee employee2 = new Employee(2, "emp02");
+		
+		WorkPeriod workedInterval1Eployee1 = new WorkPeriod(today, time(8, 0), time(12, 0), employee1);
+		WorkPeriod workedInterval2Eployee2 = new WorkPeriod(today, time(10, 0), time(15, 0), employee2);
+		
+		WorkRepository workRepository = new Work();
+		workRepository.store(workedInterval1Eployee1);
+		workRepository.store(workedInterval2Eployee2);
+	}
 
 	@Test
 	public void shouldFindStoredWorkPeriodByClient() {
@@ -151,11 +267,6 @@ public class WorkTest {
 	
 	@Test
 	public void shouldFindStoredWorkPeriodForDateIntervalAndClient() {
-		
-	}
-	
-	@Test
-	public void shouldNotAcceptWorkPeriodThatOverlapsWithExistingWorkPeriod() {
 		
 	}
 	
