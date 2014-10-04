22e5350a4079aaf959b6ad783776ffe040490982
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-10-03 21:25:01 -0300

    Constructor, hashcode and equals

--- timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/project/Client.java
+++ timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/project/Client.java
@@ -1,6 +1,53 @@
 package br.org.matrix.timesheet.project;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.base.Objects;
+
 public class Client {
 
+	private Integer id;
 	private String name;
+	
+	public Client(Integer id, String name) {
+		super();
+		
+		checkState(id!=null, "Id cannot be null.");
+		checkState(name!=null, "Name cannot be null.");
+		
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
