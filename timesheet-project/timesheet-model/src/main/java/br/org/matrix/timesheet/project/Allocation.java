6a27aa38bc14567b58f3ba1aceb28bf971176afa
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-10-16 23:26:02 -0300

    Adding new factory method

--- timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/project/Allocation.java
+++ timesheet-project/timesheet-model/src/main/java/br/org/matrix/timesheet/project/Allocation.java
@@ -25,6 +25,24 @@ public class Allocation {
 	public Project getProject() {
 		return project;
 	}
+	
+	/**
+	 * Helper method to build {@link Allocation} object from its parts.
+	 * 
+	 * @param idEmployee
+	 * @param nameEmployee
+	 * @param idProject
+	 * @param nameProject
+	 * @param idClient
+	 * @param nameClient
+	 * @return Allocation object
+	 */
+	public static Allocation createAllocation(int idEmployee, String nameEmployee, int idProject, String nameProject, int idClient, String nameClient) {
+		Employee employee = new Employee(idEmployee, nameEmployee);
+		Client client = new Client(idClient, nameClient);
+		Project project = new Project(idProject, nameProject, client);
+		return new Allocation(employee, project);
+	}
 
 	@Override
 	public int hashCode() {
