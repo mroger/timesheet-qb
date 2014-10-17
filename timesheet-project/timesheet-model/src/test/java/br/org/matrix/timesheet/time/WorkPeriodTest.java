02e182feb9c0b626a2308bad5755f00b57899a6d
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-10-16 23:29:23 -0300

    Change by method moved to Allocation class

--- timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkPeriodTest.java
+++ timesheet-project/timesheet-model/src/test/java/br/org/matrix/timesheet/time/WorkPeriodTest.java
@@ -1,7 +1,5 @@
 package br.org.matrix.timesheet.time;
 
-import static br.org.matrix.timesheet.util.TimesheetUtil.createAllocation;
-
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.junit.Test;
@@ -17,31 +15,31 @@ public class WorkPeriodTest {
 	
 	@Test(expected=IllegalStateException.class)
 	public void create_DateNotSet() {
-		Allocation allocation = createAllocation(1, "emp01", 1, "project01", 1, "client01");
+		Allocation allocation = Allocation.createAllocation(1, "emp01", 1, "project01", 1, "client01");
 		new WorkPeriod(null, now(), now(), allocation);
 	}
 
 	@Test(expected=IllegalStateException.class)
 	public void create_StartTimeNotSet() {
-		Allocation allocation = createAllocation(1, "emp01", 1, "project01", 1, "client01");
+		Allocation allocation = Allocation.createAllocation(1, "emp01", 1, "project01", 1, "client01");
 		new WorkPeriod(today(), null, now(), allocation);
 	}
 	
 	@Test(expected=IllegalStateException.class)
 	public void create_StopTimeNotSet() {
-		Allocation allocation = createAllocation(1, "emp01", 1, "project01", 1, "client01");
+		Allocation allocation = Allocation.createAllocation(1, "emp01", 1, "project01", 1, "client01");
 		new WorkPeriod(today(), now(), null, allocation);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void create_StartTimeLessThanStopTime() {
-		Allocation allocation = createAllocation(1, "emp01", 1, "project01", 1, "client01");
+		Allocation allocation = Allocation.createAllocation(1, "emp01", 1, "project01", 1, "client01");
 		new WorkPeriod(today(), time(15, 0), time(11, 0), allocation);
 	}
 	
 	@Test
 	public void create() {
-		Allocation allocation = createAllocation(1, "emp01", 1, "project01", 1, "client01");
+		Allocation allocation = Allocation.createAllocation(1, "emp01", 1, "project01", 1, "client01");
 		new WorkPeriod(today(), time(11, 0), time(15, 0), allocation);
 	}
 
